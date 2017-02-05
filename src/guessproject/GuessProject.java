39771e089f1f0e2b068a667ce61f6768873532e4
Jeanne Kagle <jkagle@mansfield.edu>
2017-02-05 12:36:50 -0500

    Modified to tell player if guess is too low or too high.

---
+++ src/guessproject/GuessProject.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guessproject;
+import java.util.Random;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author 55jkagle
+ */
+public class GuessProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        int number = rand.nextInt(10)+1;
+        
+        System.out.println("Type a number between 1 and 10");
+        int choice = Integer.parseInt(sc.nextLine());
+        
+        while (number != choice){
+            if (choice > number){
+                System.out.println("That's too high.  Try again.");
+            }
+            if (choice<number){
+                System.out.println("That's too low.  Try again.");
+            }
+            System.out.println("Type a number between 1 and 10");
+            choice = Integer.parseInt(sc.nextLine());
+        }
+        
+        System.out.println("That's it! The number was " + number+ "! Thanks for playing!");
+    
+}}
